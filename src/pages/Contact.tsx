abfd5e839b39da8db123161685685e2a69255990
Jorge Massango <jorgelili1617@gmail.com>
2025-07-28 23:15:02 +0200

    Configuracao do email

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -57,7 +57,7 @@ export default function Contact() {
         }
 
         try {
-            const response = await fetch("http://localhost:3333/contact", {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/contact`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ name, email, message }),
